7e8b5f0e1bb6d7c2512b2a67c7d900fdb03bd1ce
¯\_(ツ)_/¯ <35753883+iqzprvagbv@users.noreply.github.com>
2018-01-15 00:39:46 -0800

    Refactored core

--- path-generation/core.py
+++ path-generation/core.py
@@ -1,13 +1,26 @@
-import spline as s
+from path    import from_waypoints
+from profile import VelocityProfile
+from robot   import Robot
+from spline  import Waypoint
 
-import matplotlib.pyplot as plt
+# Width of the robot in feet
+robot_width = 2
+# Max Velocity of robot in fps
+max_velocity = 1
+# Max acceleration of robot in fps^2
+max_acceleration = 10
 
-w1 = s.Waypoint((0,0),(0,20),(1,0))
-w2 = s.Waypoint((5,5),(0,20),(-1,0))
+# Way points for the curve to hit
+# Format: Waypoint(position, velocity vector, acceleration vector)
+waypoints = []
+waypoints.append(Waypoint((0,0),(0,10),(1,0)))
+waypoints.append(Waypoint((5,5),(0,10),(-1,0)))
+waypoints.append(Waypoint((0,10),(0,10),(1,0)))
 
-x = s.from_waypoints(w1,w2)
+# Distance between planning points in feet
+ds = 0.1
 
-x.draw(plt)
-
-plt.axis('equal')
-plt.show()
+path = from_waypoints(waypoints)
+robot = Robot(robot_width,max_velocity,max_acceleration)
+vp = VelocityProfile(path,robot,ds)
+vp.draw()
