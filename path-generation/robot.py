2eda46b13ecc6ef7f41a58b9e343075c9393275d
¯\_(ツ)_/¯ <35753883+iqzprvagbv@users.noreply.github.com>
2018-01-14 22:52:28 -0800

    Added robot

---
+++ path-generation/robot.py
@@ -0,0 +1,6 @@
+#Not really sure if this deserves it's own class
+class Robot:
+    def __init__(self,width,velocity,acceleration):
+        self.width = width
+        self.max_velocity = velocity
+        self.max_acceleration = acceleration
