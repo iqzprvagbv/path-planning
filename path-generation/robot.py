388ee323150f8b3355660a13597eae806f450b52
¯\_(ツ)_/¯ <35753883+iqzprvagbv@users.noreply.github.com>
2018-01-26 03:50:07 -0600

    added string output to robot

--- path-generation/robot.py
+++ path-generation/robot.py
@@ -4,3 +4,10 @@ class Robot:
         self.width = width
         self.max_velocity = velocity
         self.max_acceleration = acceleration
+
+    def __str__(self):
+        title =  " Robot Attributes"
+        width       =  " Width       : " + str(self.width) + " feet"
+        velocity    =  " Velocity    : " + str(self.max_velocity) + " feet per second"
+        acceleration =  " Acceleration: " + str(self.max_acceleration) + " feet per second squared"
+        return title + "\n" + width + "\n" + velocity + "\n" + acceleration
